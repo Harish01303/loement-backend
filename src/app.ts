1ad14b424eed224c125d88fb0a0720d0a0648399
Harish01303 <34677592+Harish01303@users.noreply.github.com>
2026-03-17 14:02:08 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -50,7 +50,8 @@ app.use(cors({
         "http://localhost:5175",
         "http://localhost:5176",
         "https://bhoomi-graph.vercel.app",
-        "https://loement.com"
+        "https://loement.com",
+        "https://www.loement.com"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
